84cad822f44fce83a6218cc612279fbf0ae4c2ae
emiliendurif <emilien.durif@gmail.com>
2017-10-18 23:59:50 +0200

    MAJ

--- TP_simulation/ericc3/modele_matlab/data_modele_ericc.m
+++ TP_simulation/ericc3/modele_matlab/data_modele_ericc.m
@@ -6,8 +6,10 @@ Kc=2000/(2*pi);
 
 %Parametres du correcteur
 Kp=1e6;
-Ki=0;
+Ki=2E5;
 Kd=0;
+%Ki=0;
+%Kd=0;
 
 %Reducteur
 Kr=3/1000;
@@ -19,8 +21,8 @@ Ke=0.043;
 Jm=0.41*1e-5;
 R=2.3;
 L=1.1*1e-3;
-fv=9e-5;
-Jeq=2.09*1e-5;
+fv=1/8547;
+Jeq=fv*0.178;
 
 %Inertie du robot
 J1=1.76*1E3;
